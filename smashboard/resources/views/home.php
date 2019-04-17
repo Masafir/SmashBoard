ed372b8e883f27285a4c635577e434051484f9b4
Masafir-Nova <lachamir@outlook.fr>
2019-04-17 15:13:03 +0200

    recovering from the git fiasco

--- smashboard/resources/views/home.php
+++ smashboard/resources/views/home.php
@@ -1,29 +1,36 @@
 <?php require('layout/header.php'); ?>
     LA home
-    <div class="container-fluid">
-    <div class="row justify-content-center">
+    <div class="container">
+        <ul>
+    <div class="row justify-content-around">
     <?php 
     $row = 0;
     foreach($characters as $character):
         
         if($row == 3)
         {
-            echo '</div><div class="row justify-content-center">';
+            echo '</div><div class="row justify-content-around">';
             $row = 0;
             ?>
-            <div class="charact col-4  justify-content-center">
-                <img src="<?=route('home')?>/charac/<?=$character->id?>.png" alt="<?=$character->name ?>">
-            </div>
+            <li class="list_item">
+                <div class="charact">
+                    <img src="<?=route('home')?>/charac/<?=$character->id?>.png" class="character" alt="<?=$character->name ?>">
+                </div>
+            </li>
             <?php
         } 
         else {
         
     ?>
-    <div class="charact col-4  justify-content-center">
-    <img src="<?=route('home')?>/charac/<?=$character->id?>.png" class="character" alt="<?=$character->name ?>">
-    </div>
+    <li class="list_item">
+        <div class="charact rounded">
+            <img src="<?=route('home')?>/charac/<?=$character->id?>.png" class="character" alt="<?=$character->name ?>">
+        </div>
+    </li>
         <?php }
         $row ++;
         endforeach; ?>
-    </div>
+    </ul>    
+</div>
+
 <?php require('layout/footer.php'); ?>
